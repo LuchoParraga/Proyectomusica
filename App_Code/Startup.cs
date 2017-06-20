8975b673782cd9fb15b72af68772802697b9a67c
luchoparraga <luisfmon@live.com.ar>
2017-06-20 18:58:35 -0400

    VersionInicial

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyectomusica.Startup))]
+namespace Proyectomusica
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
